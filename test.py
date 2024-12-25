8ea8e96c5389581ee1cdfe7433d399242aed48e9
Azizbek Akhmedov <azizbek.akhmedov@gmail.com>
2024-12-25 20:29:30 +0500

    Add tests

---
+++ test.py
@@ -0,0 +1,12 @@
+from calculator import subtract
+
+def test_subtract(a, b):
+    c = subtract(a, b)
+    test_c = a - b
+
+    assert c == test_c
+
+
+test_subtract(20, 10)
+test_subtract(15, 1)
+test_subtract(150, 40)
